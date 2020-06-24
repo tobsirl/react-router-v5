61a52a3454f3a3242da37568244e844a32a67767
Paul Tobin <tobster111@gmail.com>
2020-06-24 20:06:39 +0100

    URL Parameters - Finished Challenge using useParams hook

--- url-parameters/src/App.js
+++ url-parameters/src/App.js
@@ -13,9 +13,15 @@ import {
   When the user navigates to /invoices/1, they should see "Invoice #1".
 */
 
-const Student = () => <h3>Student: SHOW NAME HERE</h3>;
+const Student = () => {
+  const { student } = useParams();
+  return <h3>Student: {student}</h3>;
+};
 
-const Invoice = () => <h3>Invoice #SHOW ID HERE</h3>;
+const Invoice = () => {
+  const { id } = useParams();
+  return <h3>Invoice: {id}</h3>;
+};
 
 export default function App() {
   return (
@@ -53,6 +59,12 @@ export default function App() {
         <hr />
 
         {/* Routes go here */}
+        <Route exact path="/:student">
+          <Student />
+        </Route>
+        <Route path="/invoices/:id">
+          <Invoice />
+        </Route>
       </React.Fragment>
     </Router>
   );
