71c9910c90366aaf185973543519541809cd91d2
Paul Tobin <tobster111@gmail.com>
2020-07-13 20:06:18 +0100

    Challenge complete

--- query-strings/src/App.js
+++ query-strings/src/App.js
@@ -6,43 +6,52 @@
   If no user is selected, show "Select a user".
 */
 
-import * as React from "react"
-import "./styles.css"
-import { parse } from "query-string"
+import * as React from 'react';
+import './styles.css';
+import { parse } from 'query-string';
 import {
   BrowserRouter as Router,
   Link,
   Route,
-  useLocation
-} from "react-router-dom"
+  useLocation,
+} from 'react-router-dom';
 
 const users = {
   tylermcginnis: {
-    name: "Tyler McGinnis",
-    handle: "tylermcginnis",
+    name: 'Tyler McGinnis',
+    handle: 'tylermcginnis',
     avatar:
-      "https://pbs.twimg.com/profile_images/1138547910524588034/4R6jdBch_400x400.png"
+      'https://pbs.twimg.com/profile_images/1138547910524588034/4R6jdBch_400x400.png',
   },
   sarah_edo: {
-    name: "Sarah Drasner",
-    handle: "sarah_edo",
+    name: 'Sarah Drasner',
+    handle: 'sarah_edo',
     avatar:
-      "https://pbs.twimg.com/profile_images/1225613270205091840/NyoNYuhC_400x400.jpg"
+      'https://pbs.twimg.com/profile_images/1225613270205091840/NyoNYuhC_400x400.jpg',
   },
   dan_abramov: {
-    name: "Dan Abramov",
-    handle: "dan_abramov",
+    name: 'Dan Abramov',
+    handle: 'dan_abramov',
     avatar:
-      "https://pbs.twimg.com/profile_images/1166344766210150401/amRnWzl-_400x400.jpg"
-  }
-}
+      'https://pbs.twimg.com/profile_images/1166344766210150401/amRnWzl-_400x400.jpg',
+  },
+};
 
 function User() {
+  const { search } = useLocation();
+  const { id } = parse(search);
+
+  if (!id) {
+    return <p>Select a user</p>;
+  }
+
+  const { name, handle, avatar } = users[id];
   return (
     <div className="user">
-
+      <img src={avatar} alt={`Avatar for ${name}`} />
+      <a href={`https://www.twitter.com/${handle}`}>{name}</a>
     </div>
-  )
+  );
 }
 
 export default function App() {
@@ -69,5 +78,5 @@ export default function App() {
         </Route>
       </div>
     </Router>
-  )
+  );
 }
