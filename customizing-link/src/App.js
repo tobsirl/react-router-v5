6a90cc41083b712330c2e7fa77372d75bae6484f
Paul Tobin <tobster111@gmail.com>
2020-07-20 17:52:17 +0100

    Setup challenge

---
+++ customizing-link/src/App.js
@@ -0,0 +1,48 @@
+/*
+  Your application has three routes, `/`, `/notifications`,
+  and `/settings`. 
+  
+  Your job is to implement the `EmojiLink`
+  custom `Link` component so that it renders the `Emoji`
+  along with the name (`children`) if the app's location
+  matches the `Link`s `path`.
+*/
+
+import * as React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const Home = () => <h2>Home</h2>;
+const Settings = () => <h2>Settings</h2>;
+const Notifications = () => <h2>Notifications</h2>;
+
+const EmojiLink = ({ children, to, exact, emoji }) => <div />;
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <EmojiLink exact={true} to="/" emoji={`🏠`}>
+          Home
+        </EmojiLink>
+        <EmojiLink to="/notifications" emoji={`🔔`}>
+          Notifications
+        </EmojiLink>
+        <EmojiLink to="/settings" emoji={`⚙️`}>
+          Settings
+        </EmojiLink>
+
+        <hr />
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/notifications">
+          <Notifications />
+        </Route>
+        <Route path="/settings">
+          <Settings />
+        </Route>
+      </div>
+    </Router>
+  );
+}
