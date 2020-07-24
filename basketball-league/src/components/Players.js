29014aabe92233fc07e558bccfe17163e2f84917
Paul Tobin <tobster111@gmail.com>
2020-07-24 15:49:36 +0100

    Player component, using query-string. Checking location.search for a team and requesting the data from the api

--- basketball-league/src/components/Players.js
+++ basketball-league/src/components/Players.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Sidebar from './Sidebar';
+import { useLocation } from 'react-router-dom';
+import { parse } from 'query-string';
 import usePlayers from '../hooks/usePlayers';
 
 const Players = () => {
-  const { loading, response: players } = usePlayers();
+  const location = useLocation();
+
+  const team = location.search ? parse(location.search).teamId : null;
+
+  const { loading, response: players } = usePlayers(team);
+
+  if (loading) return <p>Loading...</p>;
+
   return (
-    <div className="container">
-      <h3 className="header">Players</h3>
-      <Sidebar players={{loading, players}} />
+    <div className="container two-column">
+      <Sidebar title="Players" list={players.map((player) => player.name)} />
     </div>
   );
 };
