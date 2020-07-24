fcf9a4c4f13aaf5e5bf0484ce5ee8b16d8d8d28b
Paul Tobin <tobster111@gmail.com>
2020-07-24 16:41:56 +0100

    Rendering the Player component, using find to select and display the correct player

--- basketball-league/src/components/Players.js
+++ basketball-league/src/components/Players.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Sidebar from './Sidebar';
-import { useLocation } from 'react-router-dom';
+import {
+  Switch,
+  Route,
+  useLocation,
+  useRouteMatch,
+  useParams,
+} from 'react-router-dom';
 import { parse } from 'query-string';
 import usePlayers from '../hooks/usePlayers';
+import slug from 'slug';
+
+function Player({ players }) {
+  const { playerId } = useParams();
+  const player = players.find(({ name }) => slug(name) === playerId);
+
+  return <div>{JSON.stringify(player, null, 2)}</div>;
+}
 
 const Players = () => {
+  const { url } = useRouteMatch();
   const location = useLocation();
 
   const team = location.search ? parse(location.search).teamId : null;
@@ -16,6 +31,14 @@ const Players = () => {
   return (
     <div className="container two-column">
       <Sidebar title="Players" list={players.map((player) => player.name)} />
+      <Switch>
+        <Route path={`${url}/:playerId`}>
+          <Player players={players} />
+        </Route>
+        <Route path="*">
+          <div className="sidebar-instruction">Select a Player</div>
+        </Route>
+      </Switch>
     </div>
   );
 };
