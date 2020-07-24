e2122ea520a20d60e1fe6c1f3ca26bd5176dfcff
Paul Tobin <tobster111@gmail.com>
2020-07-24 15:33:25 +0100

    Created a custom link for the sidebar component

--- basketball-league/src/components/Players.js
+++ basketball-league/src/components/Players.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import Sidebar  from './Sidebar'
+import React from 'react';
+import Sidebar from './Sidebar';
+import usePlayers from '../hooks/usePlayers';
 
 const Players = () => {
+  const { loading, response: players } = usePlayers();
   return (
     <div className="container">
-    <h3 className="header">Players</h3>
-    <Sidebar />
-      
+      <h3 className="header">Players</h3>
+      <Sidebar players={{loading, players}} />
     </div>
-  )
-}
+  );
+};
 
-export default Players
+export default Players;
--- basketball-league/src/components/Sidebar.js
+++ basketball-league/src/components/Sidebar.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import { Link, useRouteMatch, useLocation } from 'react-router-dom';
 import slug from 'slug';
-import usePlayers from '../hooks/usePlayers';
 
-function Sidebar() {
-  const { loading, response: players } = usePlayers();
-  console.log(players);
+function CustomLink({ to, children }) {
+  const match = useRouteMatch(to.pathname);
 
-  if (loading) return null;
+  return (
+    <li style={{ fontWeight: match ? 900 : 'normal' }}>
+      <Link to={to}>{children}</Link>
+    </li>
+  );
+}
+
+function Sidebar({ title, list }) {
+  const { url } = useRouteMatch();
+  const location = useLocation();
+
   return (
     <div>
+      <h3 className="header">{title}</h3>
       <ul>
-        {players.map((player) => (
-          <li key={player.name}>
-            <Link to="">{player.name}</Link>
-          </li>
+        {list.map((item) => (
+          <CustomLink
+            key={item}
+            to={{
+              pathname: `${url}/${slug(item)}`,
+              search: location.search,
+            }}
+          >
+            {item.toUpperCase()}
+          </CustomLink>
         ))}
       </ul>
     </div>
