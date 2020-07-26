fd7bb1938f5ec95c32be3047f0e16863c8572105
Paul Tobin <tobster111@gmail.com>
2020-07-26 13:42:38 +0100

    Team component to display individual team information

--- basketball-league/src/components/Teams.js
+++ basketball-league/src/components/Teams.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Switch, Route, useRouteMatch, useParams } from 'react-router-dom';
 import useTeamNames from '../hooks/useTeamNames';
+import useTeam from '../hooks/useTeam';
 import Sidebar from './Sidebar';
 
+function Team({teams}) {
+  const {teamId} = useParams();
+  const {loading, response: team} = useTeam(teamId)
+  console.log(team);
+
+  if(loading) return <p className="text-center">Loading...</p>
+
+  return (
+    <div>
+      {JSON.stringify(team, null, 2)}
+    </div>
+  )
+
+}
+
+
+
 const Teams = () => {
+  const { url}= useRouteMatch()
   const { loading, response: teams } = useTeamNames();
-  console.log(teams);
+
 
   if(loading) return null
 
   return (
     <div className="container two-column">
-      <Sidebar title="Teams" list={teams.map((team) => team)} />  
+      <Sidebar title="Teams" list={teams.map((team) => team)} /> 
+      <Switch>
+        <Route path={`${url}/:teamId`}>
+          <Team />
+        </Route>
+      </Switch>
+
     </div>
   );
 };
