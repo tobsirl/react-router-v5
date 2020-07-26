b87a1c25c212f4266373fc88088e22cb04f982fa
Paul Tobin <tobster111@gmail.com>
2020-07-26 14:06:24 +0100

    JSX for the team component

--- basketball-league/src/components/Teams.js
+++ basketball-league/src/components/Teams.js
@@ -1,42 +1,63 @@
 import React from 'react';
-import { Link, Switch, Route, useRouteMatch, useParams } from 'react-router-dom';
+import {
+  Link,
+  Switch,
+  Route,
+  useRouteMatch,
+  useParams,
+} from 'react-router-dom';
 import useTeamNames from '../hooks/useTeamNames';
 import useTeam from '../hooks/useTeam';
 import Sidebar from './Sidebar';
+import TeamLogo from './TeamLogo';
 
-function Team({teams}) {
-  const {teamId} = useParams();
-  const {loading, response: team} = useTeam(teamId)
-  console.log(team);
+function Team() {
+  const { teamId } = useParams();
+  const { loading, response: team } = useTeam(teamId);
 
-  if(loading) return <p className="text-center">Loading...</p>
+  if (loading) return <p className="text-center">Loading...</p>;
 
   return (
-    <div>
-      {JSON.stringify(team, null, 2)}
+    <div className="panel">
+      <div style={{ width: '100%' }}>
+        <TeamLogo id={team.id} className="center" />
+        <h1 className="medium-header">{team.name}</h1>
+        <ul className="info-list row">
+          <li>
+            Est.<div>{team.established}</div>
+          </li>
+          <li>
+            Manager<div>{team.manager}</div>
+          </li>
+          <li>
+            Coach<div>{team.coach}</div>
+          </li>
+        </ul>
+        <Link className="center btn-main" to={`/${teamId}`}>
+          {team.name} Team Page
+        </Link>
+      </div>
     </div>
-  )
-
+  );
 }
 
-
-
 const Teams = () => {
-  const { url}= useRouteMatch()
-  const { loading, response: teams } = useTeamNames();
+  const { url } = useRouteMatch();
+  const { loading, response: teamNames } = useTeamNames();
 
-
-  if(loading) return null
+  if (loading) return <p className="text-center">Loading</p>;
 
   return (
     <div className="container two-column">
-      <Sidebar title="Teams" list={teams.map((team) => team)} /> 
+      <Sidebar title="Teams" list={teamNames} />
       <Switch>
         <Route path={`${url}/:teamId`}>
           <Team />
         </Route>
+        <Route path="*">
+          <div className="sidebar-instruction">Select a Team</div>
+        </Route>
       </Switch>
-
     </div>
   );
 };
