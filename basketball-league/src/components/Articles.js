b1cdb102096e7238eb16569068cc1db6c666fb73
Paul Tobin <tobster111@gmail.com>
2020-07-28 16:59:20 +0100

    Setup the Switch, Route for the Article component. Using useRouteMatch to get the url

--- basketball-league/src/components/Articles.js
+++ basketball-league/src/components/Articles.js
@@ -1,21 +1,39 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Switch, Route, useRouteMatch } from 'react-router-dom'
 import Sidebar from './Sidebar'
 
 import useArticle from '../hooks/useArticle'
 import useTeamArticles from '../hooks/useTeamArticles'
 
+function Article() {
+  const {teamId, articleId} = useParams()
+  
+  return (
+    <div>
+      Article = {teamId} - {articleId}
+    </div>
+  )
+
+}
+
 export default function Articles() {
   const {teamId} = useParams()
   const {loading, response: articles} = useTeamArticles(teamId)
+  const {url} = useRouteMatch()
 
   console.log(articles);
 
   if (loading) return <p className="text-center">Loading...</p>;
+
   return (
     <div className="container two-column">
-      Articles {teamId}
+      
       <Sidebar title="Articles" list={articles.map((article) => article.title)}/>
+      <Switch>
+        <Route path={`${url}/:articleId`}>
+          <Article />
+        </Route>
+      </Switch>
     </div>
   )
 }
