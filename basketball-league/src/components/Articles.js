fe534cb96c153bfaf908aeaa803a3fd780946889
Paul Tobin <tobster111@gmail.com>
2020-07-28 16:51:45 +0100

    Got the list of articles from useTeamArticles hook, rendered the sidebar sending a list of article titles

--- basketball-league/src/components/Articles.js
+++ basketball-league/src/components/Articles.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import Sidebar from './Sidebar'
+
+import useArticle from '../hooks/useArticle'
+import useTeamArticles from '../hooks/useTeamArticles'
 
 export default function Articles() {
   const {teamId} = useParams()
+  const {loading, response: articles} = useTeamArticles(teamId)
+
+  console.log(articles);
+
+  if (loading) return <p className="text-center">Loading...</p>;
   return (
-    <div>
+    <div className="container two-column">
       Articles {teamId}
+      <Sidebar title="Articles" list={articles.map((article) => article.title)}/>
     </div>
   )
 }
