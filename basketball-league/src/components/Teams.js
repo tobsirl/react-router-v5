f83e2a991ea7c8634321fc7bfcdd39bb4f0e7bfa
Paul Tobin <tobster111@gmail.com>
2020-07-30 17:21:44 +0100

    Added transitions to Teams.js

--- basketball-league/src/components/Teams.js
+++ basketball-league/src/components/Teams.js
@@ -5,18 +5,20 @@ import {
   Route,
   useRouteMatch,
   useParams,
+  useLocation,
 } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import useTeamNames from '../hooks/useTeamNames';
 import useTeam from '../hooks/useTeam';
 import Sidebar from './Sidebar';
 import TeamLogo from './TeamLogo';
-import Loading from './Loading'
+import Loading from './Loading';
 
 function Team() {
   const { teamId } = useParams();
   const { loading, response: team } = useTeam(teamId);
 
-  if (loading) return <Loading />
+  if (loading) return <Loading />;
 
   return (
     <div className="panel">
@@ -45,20 +47,25 @@ function Team() {
 const Teams = () => {
   const { path } = useRouteMatch();
   const { loading, response: teamNames } = useTeamNames();
+  const location = useLocation();
 
-  if (loading) return <Loading />
+  if (loading) return <Loading />;
 
   return (
     <div className="container two-column">
       <Sidebar title="Teams" list={teamNames} />
-      <Switch>
-        <Route path={`${path}/:teamId`}>
-          <Team />
-        </Route>
-        <Route path="*">
-          <div className="sidebar-instruction">Select a Team</div>
-        </Route>
-      </Switch>
+      <TransitionGroup component={null}>
+        <CSSTransition timeout={500} classNames="fade" key={location.key}>
+          <Switch location={location}>
+            <Route path={`${path}/:teamId`}>
+              <Team />
+            </Route>
+            <Route path="*">
+              <div className="sidebar-instruction">Select a Team</div>
+            </Route>
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
     </div>
   );
 };
