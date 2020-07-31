c907ae16cae5cf265a3891eaa12a08522098ebcb
Paul Tobin <tobster111@gmail.com>
2020-07-31 16:55:53 +0100

    Added React.lazy and Suspense to the project, lazy loading component at the route level

--- basketball-league/src/components/App.js
+++ basketball-league/src/components/App.js
@@ -1,39 +1,42 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Loading from './Loading';
 
 import Navbar from './Navbar';
-import Home from './Home';
-import Players from './Players';
-import Teams from './Teams';
-import TeamPage from './TeamPage';
-import FourOhFour from './FourOhFour';
-import Articles from './Articles';
+const Home = React.lazy(() => import('./Home'));
+const Players = React.lazy(() => import('./Players'));
+const Teams = React.lazy(() => import('./Teams'));
+const TeamPage = React.lazy(() => import('./TeamPage'));
+const FourOhFour = React.lazy(() => import('./FourOhFour'));
+const Articles = React.lazy(() => import('./Articles'));
 
 export default function App() {
   return (
     <Router>
       <div>
         <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/players">
-            <Players />
-          </Route>
-          <Route path="/teams">
-            <Teams />
-          </Route>
-          <Route exact path="/:teamId">
-            <TeamPage />
-          </Route>
-          <Route path="/:teamId/articles">
-            <Articles />
-          </Route>
-          <Route path="*">
-            <FourOhFour />
-          </Route>
-        </Switch>
+        <React.Suspense fallback={<Loading />}>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/players">
+              <Players />
+            </Route>
+            <Route path="/teams">
+              <Teams />
+            </Route>
+            <Route exact path="/:teamId">
+              <TeamPage />
+            </Route>
+            <Route path="/:teamId/articles">
+              <Articles />
+            </Route>
+            <Route path="*">
+              <FourOhFour />
+            </Route>
+          </Switch>
+        </React.Suspense>
       </div>
     </Router>
   );
