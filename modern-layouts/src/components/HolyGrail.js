40d0d359c258d83d6949906f4762a6812169873e
Paul Tobin <tobster111@gmail.com>
2020-08-03 19:17:29 +0100

    Added styling for Holygrail layout and Span layout

--- modern-layouts/src/components/HolyGrail.js
+++ modern-layouts/src/components/HolyGrail.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 export default function HolyGrail() {
   return (
-    <div>
-      05. Classic Holy Grail Layout
-    </div>
-  )
+    <React.Fragment>
+      <body className="ex5">
+        <header className="ex5">
+          <h1 contenteditable>Header.com</h1>
+        </header>
+        <div class="left-sidebar ex5" contenteditable>
+          Left Sidebar
+        </div>
+        <main contenteditable className="ex5"></main>
+        <div class="right-sidebar ex5" contenteditable>
+          Right Sidebar
+        </div>
+        <footer contenteditable className="ex5">
+          Footer Content — Header.com 2020
+        </footer>
+      </body>
+    </React.Fragment>
+  );
 }
--- modern-layouts/src/components/Span.js
+++ modern-layouts/src/components/Span.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 export default function Span() {
   return (
-    <div>
-      06. 12-Span Grid
-    </div>
-  )
+    <React.Fragment>
+      <body className="ex6">
+        <div className="span-12 ex6">Span 12</div>
+        <div className="span-6 ex6">Span 6</div>
+        <div className="span-4 ex6">Span 4</div>
+        <div className="span-2 ex6">Span 2</div>
+      </body>
+    </React.Fragment>
+  );
 }
