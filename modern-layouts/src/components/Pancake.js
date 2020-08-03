104f7a65403039a1ceea388f99a85d953d406833
Paul Tobin <tobster111@gmail.com>
2020-08-03 19:04:36 +0100

    Added styling for Sidebar layout and Pancake layout

--- modern-layouts/src/components/Pancake.js
+++ modern-layouts/src/components/Pancake.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 export default function Pancake() {
   return (
-    <div>
-      04. Pancake Stack
-    </div>
-  )
+    <>
+      <body className="ex4">
+        <header className="ex4">
+          <h1>Header.com</h1>
+        </header>
+        <main className="ex4"></main>
+        <footer className="ex4">Footer Content — Header.com 2020</footer>
+      </body>
+    </>
+  );
 }
--- modern-layouts/src/components/Sidebar.js
+++ modern-layouts/src/components/Sidebar.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 export default function Sidebar() {
   return (
-    <div>
-      03. Sidebar Says
-    </div>
-  )
+    <React.Fragment>
+      <body className="ex3">
+        <div className="sidebar ex3" contenteditable>
+          Min: 150px
+          <br />
+          Max: 25%
+        </div>
+        <p className="content ex3" contenteditable>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis nulla
+          architecto maxime modi nisi. Quas saepe dolorum, architecto quia fugit
+          nulla! Natus, iure eveniet ex iusto tempora animi quibusdam porro?
+        </p>
+      </body>
+    </React.Fragment>
+  );
 }
